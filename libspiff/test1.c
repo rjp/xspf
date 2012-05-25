ecc03458c8cd4ac55d1c7917502109572e3b8093
rjp <zimpenfish@gmail.com>
2012-05-26 00:30:50 +0100

    Blank libspiff template program

---
+++ libspiff/test1.c
@@ -0,0 +1,13 @@
+/* standard C boilerplate */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* libspiff routines for C */
+#include <xspf_c.h>
+
+int
+main (int argc, char **argv)
+{
+    return 0;
+} 
