8a9231306bf4e609998ed1bbf248646768c1cc17
rjp <zimpenfish@gmail.com>
2012-05-26 01:14:44 +0100

    Parse one of the Spotify-generated playlists

--- libspiff/test1.c
+++ libspiff/test1.c
@@ -6,8 +6,15 @@
 /* libspiff routines for C */
 #include <spiff/spiff_c.h>
 
+#define SAMPLE_PLAYLIST "/Users/rjp/Dropbox/Spotify/nokogiri-zimpenfish-fb8c895d4024351c8b5e5d87239f5ad002-Piano.xspf"
+
 int
 main (int argc, char **argv)
 {
+    struct spiff_list *playlist;
+
+    playlist = spiff_parse(SAMPLE_PLAYLIST, "http://frottage.org");
+    printf("pl=%p\n", playlist);
+
     return 0;
 } 
